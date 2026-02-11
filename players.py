fa9b813d9c90d2ec47f9d620a03e2cbe598cba3a
Ser6ii <wendyduoduo888@gmail.com>
2026-02-11 01:40:08 -0500

    added def for medics (unfinished)

--- players.py
+++ players.py
@@ -1,3 +1,5 @@
+from cities import city_list
+
 COLOUR_INDEX = {"Blue": 0, "Yellow": 1, "Black": 2, "Red": 3}
 MAX_RESEARCH_STATIONS = 6
 
@@ -10,19 +12,14 @@ class Player:
         self.city = "Atlanta"
         self.colour = colour
         self.actions = 4
-        self.require_to_cure = 5
-        self.operations = False
-        
+    
+
     def draw_cards(self, city_cards):
-        if len(self.cards) > 7:
-            pass
-        
-        else:
+        if len(self.cards) <= 7:
             for i in range(2):
                 drawn_card = city_cards.pop()
                 self.cards.append(drawn_card)
                 print(self.cards)
-        
 
     def drive_ferry(self, target_city_name, city_objects):
         if self.actions > 0:
@@ -80,7 +77,7 @@ class Player:
             if current and current.colour == colour and current.virus > 0:
                 idx = COLOUR_INDEX.get(colour)
                 if idx is not None:
-                    if board.cures[idx] or self.role == 'medic':
+                    if board.cures[idx]:
                         current.virus = 0
                         total = sum(c.virus for c in city_objects.values() if c.colour == colour)
                         if total == 0:
@@ -91,21 +88,22 @@ class Player:
 
     def share_knowledge(self, other_player, card, give, city_objects):
         if self.actions > 0:
-            if other_player.city == self.city and card == self.city:
-                if give and card in self.cards:
-                    self.cards.remove(card)
-                    other_player.cards.append(card)
-                    self.actions -= 1
-                elif not give and card in other_player.cards:
-                    other_player.cards.remove(card)
-                    self.cards.append(card)
-                    self.actions -= 1
+            if other_player.city != self.city:
+                return
+            if give and card in self.cards and card == self.city:
+                self.cards.remove(card)
+                other_player.cards.append(card)
+                self.actions -= 1
+            elif not give and card in other_player.cards and card == self.city:
+                other_player.cards.remove(card)
+                self.cards.append(card)
+                self.actions -= 1
 
     def discover_cure(self, colour, cards_to_discard, city_objects, board):
         if self.actions > 0:
             current = city_objects.get(self.city)
             idx = COLOUR_INDEX.get(colour)
-            required = self.require_to_cure
+            required = 5
             if current and current.research_center and not board.cures[idx]:
                 if len(cards_to_discard) == required and all(c in self.cards for c in cards_to_discard):
                     if all(city_objects.get(c).colour == colour for c in cards_to_discard):
@@ -123,7 +121,25 @@ class Player:
 
 
 class Medic(Player):
-    pass
+    def treat_disease(self, colour, city_objects, board):
+        if self.actions > 0:
+            current = city_objects.get(self.city)
+            if current and current.colour == colour and current.virus > 0:
+                idx = COLOUR_INDEX.get(colour)
+                current.virus = 0
+                total = sum(c.virus for c in city_objects.values() if c.colour == colour)
+                if total == 0:
+                    board.eradicated[idx] = True
+                self.actions -= 1
+
+    def auto_remove_cured_cubes(self, city_objects, board):
+        current = city_objects.get(self.city)
+        idx = COLOUR_INDEX.get(current.colour)
+        if board.cures[idx] and current.virus > 0:
+            current.virus = 0
+            total = sum(c.virus for c in city_objects.values() if c.colour == current.colour)
+            if total == 0:
+                board.eradicated[idx] = True
 
 class Scientist(Player):
         Player.require_to_cure = 4
